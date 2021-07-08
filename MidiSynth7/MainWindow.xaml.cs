e4f5f66985cbf802a8076963493e0e10d2029387
rmsoft1 <rmsoftware1@gmail.com>
2021-07-07 22:28:45 -0400

    Window rendering

--- MidiSynth7/MainWindow.xaml.cs
+++ MidiSynth7/MainWindow.xaml.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Interop;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
@@ -22,6 +24,22 @@ namespace MidiSynth7
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        #region External API
+        public const int WM_CLBUTTONDOWN = 0xA1;
+        public const int HT_CAPTION = 0x2;
+
+        [DllImport("user32.dll")]
+        public static extern int SendMessage(IntPtr hw, int message, int wp, int lp);
+
+        [DllImport("user32.dll")]
+        public static extern bool ReleaseCapture();
+        #endregion
+
+        private void GR_Title_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            SendMessage(new WindowInteropHelper(this).Handle, WM_CLBUTTONDOWN, HT_CAPTION, 0);
         }
     }
 }
