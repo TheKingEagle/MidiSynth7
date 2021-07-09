234959090499c9d9e89a53f7cfd409c7c8594194
rmsoft1 <rmsoftware1@gmail.com>
2021-07-08 23:55:54 -0400

    versioning

--- MidiSynth7/Properties/AssemblyInfo.cs
+++ MidiSynth7/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("MidiSynth7")]
-[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyCopyright("Copyright © RMSoftware Development 2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.57.57")]
-[assembly: AssemblyFileVersion("1.0.57.57")]
+[assembly: AssemblyVersion("7.0.1.58")]
+[assembly: AssemblyFileVersion("7.0.1.58")]
