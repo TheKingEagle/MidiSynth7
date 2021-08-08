f73da27c50157b2b72e55f9d6b382d99599b2340
rmsoft1 <rmsoftware1@gmail.com>
2021-08-08 10:15:39 -0400

    Project Bump v7.0.405.462

--- MidiSynth7/Properties/AssemblyInfo.cs
+++ MidiSynth7/Properties/AssemblyInfo.cs
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("7.0.399.456")]
-[assembly: AssemblyFileVersion("7.0.399.456")]
+[assembly: AssemblyVersion("7.0.405.462")]
+[assembly: AssemblyFileVersion("7.0.405.462")]
