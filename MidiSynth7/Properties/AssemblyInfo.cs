db282762f328eed677341da921287c15792401e2
rmsoft1 <rmsoftware1@gmail.com>
2021-08-29 18:25:57 -0400

    Bump 494

--- MidiSynth7/Properties/AssemblyInfo.cs
+++ MidiSynth7/Properties/AssemblyInfo.cs
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("7.0.422.479")]
-[assembly: AssemblyFileVersion("7.0.422.479")]
+[assembly: AssemblyVersion("7.0.437.494")]
+[assembly: AssemblyFileVersion("7.0.437.494")]
