405095be2e4637ce3bec2c79521c1b51a0c4120e
rmsoft1 <rmsoftware1@gmail.com>
2021-08-29 18:24:36 -0400

    Create SynthKey Interface

---
+++ MidiSynth7/components/SynthKey.cs
@@ -0,0 +1,35 @@
+﻿using MidiSynth7.entities.controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace MidiSynth7.components
+{
+    public interface ISynthKey
+    {
+        int KeyID { get; set; }
+        string NoteText { get; set; }
+
+        event EventHandler<PKeyEventArgs> VKeyUp;
+        
+        event EventHandler<PKeyEventArgs> VKeyDown;
+
+        void SendOn();
+
+        void SendOff();
+
+        void FSendOn();
+
+        void FSendOff();
+
+        void FSendOnA();
+
+        void FSendOnC(Brush background);
+
+        void SetLetter(string NoteID);
+
+    }
+}
