c5d0784e926a03d71ef8970655f4643dfd58fb9e
rmsoft1 <rmsoftware1@gmail.com>
2021-09-05 20:29:32 -0400

    v7.0.456.513

--- MidiSynth7/Properties/AssemblyInfo.cs
+++ MidiSynth7/Properties/AssemblyInfo.cs
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("7.0.440.497")]
-[assembly: AssemblyFileVersion("7.0.440.497")]
+[assembly: AssemblyVersion("7.0.456.513")]
+[assembly: AssemblyFileVersion("7.0.456.513")]
