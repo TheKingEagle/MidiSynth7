503344790f18d1962b5a0da2f62dbc7b112651cf
rmsoft1 <rmsoftware1@gmail.com>
2022-08-28 07:32:21 -0400

    QOL: Added right-click activated text box on dial

--- MidiSynth7/entities/controls/DialControl.xaml.cs
+++ MidiSynth7/entities/controls/DialControl.xaml.cs
@@ -1,75 +1,86 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Runtime.InteropServices;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using System.Runtime.InteropServices;
 using System.Windows.Threading;
-using MidiSynth7.entities;
+
 namespace MidiSynth7.entities.controls
 {
     /// <summary>
-    /// Interaction logic for DialControl.xaml
+    /// A Dial control is key to a cool looking synth.
     /// </summary>
     public partial class DialControl : UserControl
     {
-        struct cursorpos
-        {
-            public int X, Y;
-        }
-        [DllImport("user32.dll")]
-        static extern void SetCursorPos(int x, int y);
+        #region Fields
+        public static readonly DependencyProperty MaximumProperty = DependencyProperty.Register(
+          "Maximum",
+          typeof(int),
+          typeof(DialControl),
+          new PropertyMetadata(255, new PropertyChangedCallback(DC_DPChanged)));
+        public static readonly DependencyProperty MinimumProperty = DependencyProperty.Register(
+          "Minimum",
+          typeof(int),
+          typeof(DialControl),
+          new PropertyMetadata(0, new PropertyChangedCallback(DC_DPChanged)));
+        public static readonly DependencyProperty ValueProperty = DependencyProperty.Register(
+          "Value",
+          typeof(int),
+          typeof(DialControl),
+          new PropertyMetadata(0, new PropertyChangedCallback(DC_DPChanged)));
 
-        [DllImport("user32.dll")]
-        static extern void GetCursorPos(out cursorpos lpoint);
+        private DispatcherTimer t = new DispatcherTimer();
 
-        [DllImport("user32.dll")]
-        static extern void ClipCursor(ref System.Drawing.Rectangle rect);
-
-        [DllImport("user32.dll")]
-        public static extern short GetAsyncKeyState(UInt16 virtualKeyCode);
+        private bool isDown = false;
+        private int mouseY;
+        private bool suppressValueChange = true;
+        private float addto = 0;
+        #endregion
 
         public DialControl()
         {
             InitializeComponent();
             t.Interval = TimeSpan.FromMilliseconds(1);
             t.Tick += Tick;
-            Maximum = 255;
-
-
+            tx_Value.IsHitTestVisible = false;
+            tx_Value.Visibility = Visibility.Collapsed;
+            la_Value.IsHitTestVisible = false;
 
         }
-        public int TickDelay { get; set; }
-        float addto = 0;
-        void Tick(object sender, EventArgs e)
+
+        #region Delagates
+        private static void DC_DPChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            DialControl i = d as DialControl;
+            if (i == null) { return; };
+            float percent = ((float)i.Value - i.Minimum) / ((float)i.Maximum - i.Minimum);
+            percent = 1 - percent;
+            float CalculatedAngle = percent * 230 + 128;
+            float CalculatedAngle2 = (CalculatedAngle * 2) + (-CalculatedAngle);
+            i.pi_Progressbar.Angle = (int)CalculatedAngle2;
+            i.CanvasRotateTransform.Angle = -CalculatedAngle2 + 128;
+            i.la_Value.Content = i.Value;
+        }
+
+        private void Tick(object sender, EventArgs e)
         {
             if (TickDelay < 1)
             {
                 TickDelay = 1;
             }
-            //Console.WriteLine(GetAsyncKeyState(0x01));
             isDown = GetAsyncKeyState(0x01) < 0;
             if (!isDown)
             {
                 t.Stop();
-                Mouse.OverrideCursor = this.Cursor;
+                Mouse.OverrideCursor = Cursor;
             }
             if (!isDown)
             {
                 return;
             }
             suppressValueChange = false;
-            cursorpos cp = new cursorpos();
-            GetCursorPos(out cp);
+            GetCursorPos(out cursorpos cp);
             if (cp.Y < mouseY)
             {
                 if (Value < Maximum)
@@ -82,130 +93,32 @@ namespace MidiSynth7.entities.controls
                         Value += (int)(addto / TickDelay);
                         addto = 0;
                     }
-                    //set
                     mouseY = cp.Y;
                 }
                 la_Value.Content = Value;
-                //pi_Progressbar.Angle--;
-                //pi_Progressbar.Rotation--;
             }
             if (cp.Y > mouseY)
             {
-                if (Value > 0)
+                if (Value > Minimum)
                 {
-
                     addto += mouseY - cp.Y;
                     if (addto <= -TickDelay)
                     {
-
                         Value += (int)(addto / TickDelay);
                         addto = 0;
                     }
-                    //set
                     mouseY = cp.Y;
                 }
                 la_Value.Content = Value;
-                //pi_Progressbar.Angle--;
-                //pi_Progressbar.Rotation--;
+
             }
-            //if (cp.X < mouseX)
-            //{
-            //    if (Value > 0)
-            //    {
-            //        addto += (float)1 / ((float)TickDelay + 1);
-            //        Value-=(int)addto;
-            //    }
-            //    la_Value.Content = Value;
-            //   // pi_Progressbar.Angle++;
-            //   // pi_Progressbar.Rotation++;
-            //}
-
-            //Value = -((int)(((pi_Progressbar.Angle-128) / (128-358)) * (double)-Maximum))+Maximum;
-
-            //pi_Progressbar.Angle = ((int)(((_value + 128) / (128 + 358)) * (double)+Maximum)) - Maximum;
-            //SetCursorPos(mouseX, mouseY);
-            if (Value < 0) Value = 0;
+            if (Value < Minimum) Value = Minimum;
             if (Value > Maximum) Value = Maximum;
-        }
-
-        public void SetValueUnsuppressed(int value)
-        {
-            if (value > Maximum)
-            {
-                return;
-            }
-            if (value < 0)
-            {
-                return;
-            }
-            suppressValueChange = false;
-            Value = value;
-        }
-        protected virtual void onValueChange(EventArgs e)
-        {
-            EventHandler<EventArgs> temp = ValueChanged;
-            if (temp != null)
-            {
-                temp(this, e);
-            }
-        }
-        bool suppressValueChange = true;
-        public string Text { get { return (string)la_CtrlID.Content; } set { la_CtrlID.Content = value; } }
-        public int Value
-        {
-            get {
-                if (_value > _max) { Value = _max; return _value; }
-                if (_value < 0) { Value = 0; return _value; }
-                return _value;
-            }
-            set
-            {
-                _value = value; la_Value.Content = value;
-                float percent = (float)_value / (float)Maximum;
-                percent = 1 - percent;
-                float CalculatedAngle = percent * 230 + 128;
-
-                float CalculatedAngle2 = (CalculatedAngle * 2) + (-CalculatedAngle);
-                pi_Progressbar.Angle = (int)CalculatedAngle2;
-
-                CanvasRotateTransform.Angle = -CalculatedAngle2 + 128;
-                if (!suppressValueChange)
-                {
-                    onValueChange(new EventArgs());
-                    suppressValueChange = true;
-                }
-            }
-        }
-        public int Maximum
-        {
-            get { return _max; }
-            set
-            {
-                float percent = (float)Value / (float)value;
-                percent = 1 - percent;
-                float CalculatedAngle = percent * 230 + 128;
-
-                float CalculatedAngle2 = CalculatedAngle;
-                pi_Progressbar.Angle = (int)CalculatedAngle2;
-                _max = value;
-            }
-        }
-        DispatcherTimer t = new DispatcherTimer();
-
-        int mouseX, mouseY;
-        int _value = 0;
-
-        private void el_dial_KeyDown(object sender, KeyEventArgs e)
-        {
-
-
         }
 
         private void el_dial_MouseDown(object sender, MouseButtonEventArgs e)
         {
-            cursorpos cp = new cursorpos();
-            GetCursorPos(out cp);
-            mouseX = cp.X;
+            GetCursorPos(out cursorpos cp);
             mouseY = cp.Y;
             isDown = true;
             t.Start();
@@ -213,24 +126,60 @@ namespace MidiSynth7.entities.controls
 
         }
 
-        private void el_dial_MouseMove(object sender, MouseEventArgs e)
-        {
-
-        }
-        bool isDown = false;
-        private int _max;
         private void el_dial_MouseUp(object sender, MouseButtonEventArgs e)
         {
-
             t.Stop();
             isDown = false;
-            Mouse.OverrideCursor = this.Cursor;
-
-
+            Mouse.OverrideCursor = Cursor;
         }
 
+        private void el_dial_MouseRightButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            if (!tx_Value.IsHitTestVisible)
+            {
+                tx_Value.IsHitTestVisible = true;
+                tx_Value.Text = Value.ToString();
+                tx_Value.Visibility = Visibility.Visible;
+                tx_Value.IsEnabled = true;
+                tx_Value.Focusable = true;
+                tx_Value.Focus();
+                tx_Value.SelectAll();
+                InputCaptured = true;
+                TextPromptStateChanged?.Invoke(this, new EventArgs());
+            }
+        }
 
-        public event EventHandler<EventArgs> ValueChanged;
+        private void tx_Value_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter || e.Key == Key.Escape)
+            {
+                if (!int.TryParse(tx_Value.Text, out int v))
+                {
+                    System.Media.SystemSounds.Hand.Play();//wow OG shit right here
+                    tx_Value.Text = Value.ToString();
+                    tx_Value.SelectAll();
+                    return;
+                }
+                else
+                {
+                    if (v < Minimum || v > Maximum)
+                    {
+                        System.Media.SystemSounds.Hand.Play();//wow OG shit right here
+                        tx_Value.Text = Value.ToString();
+                        tx_Value.SelectAll();
+
+                        return;
+                    }
+                    SetValueUnsuppressed(v);//why do I do the things I do?
+                }
+                tx_Value.Visibility = Visibility.Collapsed;
+                tx_Value.Focusable = false;
+                tx_Value.IsEnabled = false;
+                tx_Value.IsHitTestVisible = false;
+                InputCaptured = false;
+                TextPromptStateChanged?.Invoke(this, new EventArgs());
+            }
+        }
 
         private void UserControl_IsEnabledChanged(object sender, DependencyPropertyChangedEventArgs e)
         {
@@ -241,14 +190,80 @@ namespace MidiSynth7.entities.controls
             la_CtrlID.Foreground = new SolidColorBrush(L);
         }
 
+        #endregion
 
+        #region Events
+        public event EventHandler<EventArgs> ValueChanged;
+        public event EventHandler<EventArgs> TextPromptStateChanged;
+        #endregion
 
+        #region Properties
+        public int Maximum
+        {
+            get => (int)GetValue(MaximumProperty);
+            set => SetValue(MaximumProperty, value);
+        }
+        public int Minimum
+        {
+            get => (int)GetValue(MinimumProperty);
+            set => SetValue(MinimumProperty, value);
+        }
+        public int Value
+        {
+            get => (int)GetValue(ValueProperty);
+            set
+            {
+                if (value > Maximum) value = Maximum;
+                if (value < Minimum) value = Minimum;
+                SetValue(ValueProperty, value);
+                if (!suppressValueChange)
+                {
+                    ValueChanged?.Invoke(this, new EventArgs());
+                    suppressValueChange = true;
+                }
+            }
+        }
+        public string Text { get { return (string)la_CtrlID.Content; } set { la_CtrlID.Content = value; } }
+        public bool InputCaptured { get; private set; } = false;
+        public int TickDelay { get; set; }
+        #endregion
 
+        #region P/invoke Methods
+        [DllImport("user32.dll")]
+        static extern void SetCursorPos(int x, int y);
 
+        [DllImport("user32.dll")]
+        static extern void GetCursorPos(out cursorpos lpoint);
 
+        [DllImport("user32.dll")]
+        static extern void ClipCursor(ref System.Drawing.Rectangle rect);
 
+        [DllImport("user32.dll")]
+        public static extern short GetAsyncKeyState(UInt16 virtualKeyCode);
 
+        #endregion
 
+        #region Public Methods
+        public void SetValueUnsuppressed(int value)
+        {
+            if (value > Maximum)
+            {
+                return;
+            }
+            if (value < Minimum)
+            {
+                return;
+            }
+            suppressValueChange = false;
+            Value = value;
+        }
+        #endregion
 
+        #region Structs
+        struct cursorpos
+        {
+            public int X, Y;
+        }
+        #endregion
     }
 }
