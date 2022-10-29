92daea7bccf4a59ae834c5e7a1b082926f442cde
rmsoft1 <rmsoftware1@gmail.com>
2022-10-29 12:38:56 -0400

    lord help save this project

--- MidiSynth7/Properties/AssemblyInfo.cs
+++ MidiSynth7/Properties/AssemblyInfo.cs
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("7.0.1951.2008")]
-[assembly: AssemblyFileVersion("7.0.1951.2008")]
+[assembly: AssemblyVersion("7.0.1983.2040")]
+[assembly: AssemblyFileVersion("7.0.1983.2040")]
