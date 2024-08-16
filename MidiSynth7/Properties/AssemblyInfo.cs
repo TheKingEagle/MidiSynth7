6107209e5a51a93866a95938cd457303c7e5a6a5
rmsoft1 <rmsoftware1@gmail.com>
2024-08-16 07:50:13 -0400

    Attributions

--- MidiSynth7/Properties/AssemblyInfo.cs
+++ MidiSynth7/Properties/AssemblyInfo.cs
@@ -51,7 +51,7 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("7.0.2121.2178")]
-[assembly: AssemblyFileVersion("7.0.2121.2178")]
+[assembly: AssemblyVersion("7.0.2122.2179")]
+[assembly: AssemblyFileVersion("7.0.2122.2179")]
 
-[assembly: AssemblyInformationalVersion("0.0.46.46")]
+[assembly: AssemblyInformationalVersion("0.0.47.47")]
