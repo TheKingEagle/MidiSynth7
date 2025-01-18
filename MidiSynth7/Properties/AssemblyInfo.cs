e01a418d2a951b68d0b3acbbcdeafe6d49db2fd5
rmsoft1 <rmsoftware1@gmail.com>
2025-01-18 10:53:24 -0500

    build 2258

--- MidiSynth7/Properties/AssemblyInfo.cs
+++ MidiSynth7/Properties/AssemblyInfo.cs
@@ -51,7 +51,7 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("7.0.2137.2194")]
-[assembly: AssemblyFileVersion("7.0.2137.2194")]
+[assembly: AssemblyVersion("7.0.2201.2258")]
+[assembly: AssemblyFileVersion("7.0.2201.2258")]
 
-[assembly: AssemblyInformationalVersion("0.0.62.62")]
+[assembly: AssemblyInformationalVersion("0.0.126.126")]
